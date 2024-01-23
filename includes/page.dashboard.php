481a94d128f7dfdf354d13f559bdaa22f9e46f0c
Greg Ross <greg@toolstack.com>
2024-01-23 15:09:57 -0500

    Add striping to dashboard table.

--- includes/page.dashboard.php
+++ includes/page.dashboard.php
@@ -2,13 +2,13 @@
 		GLOBAL $wpdb, $_POST, $SULlyUtils;
 
 		$TableName = $wpdb->prefix . "SULly";
-		
+
 		// If the user has selected an item to delete, delete it from the database.
 		if( array_key_exists('SULlyDeleteItem', $_GET) )
 			{
 			$wpdb->delete( $TableName, array( 'id' => $_GET['SULlyDeleteItem'] ) );
 			}
-			
+
 		// If the user has added a manual entry, add it to the database.
 		if( array_key_exists('manualadd', $_GET) )
 			{
@@ -19,7 +19,7 @@
 			else
 				{
 				$wpdb->insert( $TableName, array( 'type' => $_POST['SULlyMAType'], 'version' => $_POST['SULlyMAVersion'], 'changelog' => $_POST['SULlyMAChangeLog'], 'itemname' => $_POST['SULlyMAItem'], 'nicename' => $_POST['SULlyMAItem'], 'filename' => 'Manual', 'itemurl' => 'Manual' ) );
-				
+
 				echo "<div class='updated settings-error'><p><strong>" . __('Manual item added!', 'sully') . "</strong></p></div>\n";
 				}
 			}
@@ -32,7 +32,7 @@
 
 		// Update the database for any updates that have happened to ourselves.
 		SULlyUpdateMyself();
-		
+
 		// Check for any changes to the system
 		SULlyUpdateSystemSettings( SULlyGetSystemInfo(), unserialize( get_option( 'SULly_System_Settings' ) ) );
 
@@ -43,15 +43,15 @@
 		$curpage = 1;
 		if( isset( $_GET["pagenum"] ) ) { $curpage = $_GET["pagenum"]; }
 		if( $curpage < 1 ) { $curpage = 1; }
-		
+
 		// Determine the first entry we're going to display.
 		$pagestart = ( $curpage - 1 ) * $NumToDisplay;
 		if( $pagestart < 1 ) { $pagestart = 0; }
-		
+
 		// Select the required rows from the database.
 		$Rows = $wpdb->get_results( "SELECT * FROM $TableName ORDER BY time desc LIMIT " . $pagestart . "," . $NumToDisplay );
 		$NumRows = $wpdb->num_rows;
-		
+
 		echo "<div class='wrap'>\r\n";
 		echo "<h2>SULly - " . __('System Update Logger', 'sully') . "</h2><br>\r\n";
 
@@ -69,20 +69,20 @@
 
 		echo '<br>' . "\r\n";
 
-		echo '<table class="wp-list-table widefat fixed"><thead><tr><th>' . __('Time', 'sully') . '</th><th>' . __('Type', 'sully') . '</th><th>' . __('Item', 'sully') . '</th><th>' . __('Version', 'sully') . '</th><th>' . __('Change Log', 'sully') . '</th><th>' . __('Options', 'sully') . '</th></tr></thead>' . "\r\n";
+		echo '<table class="wp-list-table widefat fixed striped"><thead><tr><th>' . __('Time', 'sully') . '</th><th>' . __('Type', 'sully') . '</th><th>' . __('Item', 'sully') . '</th><th>' . __('Version', 'sully') . '</th><th>' . __('Change Log', 'sully') . '</th><th>' . __('Options', 'sully') . '</th></tr></thead>' . "\r\n";
 		foreach( $Rows as $CurRow )
 			{
 			echo '<tr>' . "\r\n";
 			echo '<td valign="top">' . "\r\n";
-			
+
 			$phptime = strtotime( $CurRow->time );
-			
-			echo date( get_option('time_format'), $phptime ); 
+
+			echo date( get_option('time_format'), $phptime );
 			echo '<br>' . "\r\n";
-			echo date( get_option('date_format'), $phptime ); 
-			
+			echo date( get_option('date_format'), $phptime );
+
 			echo '</td>' . "\r\n";
-			
+
 			$TypeDesc = __("Unknown", 'sully');
 			if( $CurRow->type == 'C' ) { $TypeDesc = __('WordPress Core', 'sully'); }
 			if( $CurRow->type == 'T' ) { $TypeDesc = __('Theme', 'sully'); }
@@ -92,7 +92,7 @@
 			echo '<td valign="top">' . $TypeDesc . "</td>\r\n";
 			echo '<td valign="top"><a href="' . $CurRow->itemurl . '" target="_blank">' . $CurRow->nicename . '</a></td>' . "\r\n";
 			echo '<td valign="top">' . $CurRow->version . '</td>' . "\r\n";
-			if( $CurRow->type != '' ) 
+			if( $CurRow->type != '' )
 				{
 				echo '<td valign="top" width="50%">' . preg_replace( '/\n/', '<br>', $CurRow->changelog ). '</td>' . "\r\n";
 				}
@@ -100,7 +100,7 @@
 				{
 				echo '<td valign="top" width="50%">' . $CurRow->filename . '</td>' . "\r\n";
 				}
-				
+
 
 			$alertbox = 'if( confirm(\'' . __('Really delete this item?', 'sully') . '\') ) { window.location = \'index.php?page=SULlyDashboard&SULlyDeleteItem=' . $CurRow->id . '\'; }';
 
@@ -108,13 +108,13 @@
 
 			echo '</tr>' . "\r\n";
 			}
-		
+
 		// Determine what page the "previous page" button should take us to.
 		$lastpage = $curpage - 1;
 		if( $lastpage < 1 ) { $lastpage = 1; }
-		
+
 		echo '<tfoot><tr><th colspan="6" style="text-align: center">' . "\r\n";
-		
+
 		// If we're on the first page, don't activate the previous button.
 		if( $curpage == 1 )
 			{
@@ -127,17 +127,17 @@
 
 		// Firgure out the number of rows are in the database.
 		$CountRows = $wpdb->get_results( 'SELECT COUNT(*) FROM ' . $TableName, ARRAY_N );
-		
+
 		// Add the current and total page count.
 		$displaycount = $pagestart + $NumToDisplay;
 		if( $pagestart == 0 ) { $pagestart = 1; }
 		if( $displaycount > $CountRows[0][0] ) { $displaycount = $CountRows[0][0]; }
-		
+
 		echo '&nbsp;&nbsp;&nbsp;&nbsp;Records ' . $pagestart . '-' . $displaycount . ' of ' . $CountRows[0][0] . '&nbsp;&nbsp;&nbsp;&nbsp;';
-			
+
 		// If we're on the last page, disable the "next page" button
 		if( $NumRows < $NumToDisplay )
-			{			
+			{
 			echo '<a class="button">' . __('Next', 'sully') . '</a>' . "\r\n";
 			}
 		else
@@ -145,6 +145,6 @@
 			$nextpage = $curpage + 1;
 			echo '<a class="button-primary" href="index.php?page=SULlyDashboard&pagenum=' . $nextpage . '">' . __('Next', 'sully') . '</a>';
 			}
-			
+
 		echo '</th></tr></tfoot></table></div>' . "\r\n";
 ?>
