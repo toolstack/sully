06504d96a3418c0a28f26db72e8897fb95d59033
Greg Ross <greg@toolstack.com>
2024-05-24 13:11:14 -0400

    More regex updates

--- sully.php
+++ sully.php
@@ -44,7 +44,7 @@ if( !function_exists( 'SULlyLoad' ) )
 	function SULlyAddLinksToChangeLog( $text )
 		{
 		// this function deserves credit to the fine folks at phpbb.com
-		$text = preg_replace( '#(script|about|applet|activex|chrome):#is', "\\1:", $text );
+		$text = preg_replace( '#(script|about|applet|activex|chrome):#iU', "\\1:", $text );
 
 		// pad it with a space so we can match things at the start of the 1st line.
 		$ret = ' ' . $text;
@@ -52,22 +52,22 @@ if( !function_exists( 'SULlyLoad' ) )
 		// matches an "xxxx://yyyy" URL at the start of a line, or after a space.
 		// xxxx can only be alpha characters.
 		// yyyy is anything up to the first space, newline, comma, double quote or <
-		$ret = preg_replace( "#(^|[\n ])([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $ret );
+		$ret = preg_replace( "#(^|[\n ])([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)#iU", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $ret );
 
 		// matches an "[desc](xxxx://yyyy)" markdown style links.
 		// xxxx can only be alpha characters.
 		// yyyy is anything up to the first space, newline, comma, double quote or <
-		$ret = preg_replace( "#\[(.*)\]\(([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)\)#is", "<a href=\"\\2\" target=\"_blank\">\\1</a>", $ret );
+		$ret = preg_replace( "#\[(.*)\]\(([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)\)#iU", "<a href=\"\\2\" target=\"_blank\">\\1</a>", $ret );
 
 		// matches a "www|ftp.xxxx.yyyy[/zzzz]" kinda lazy URL thing
 		// Must contain at least 2 dots. xxxx contains either alphanum, or "-"
 		// zzzz is optional.. will contain everything up to the first space, newline,
 		// comma, double quote or <.
-		$ret = preg_replace( "#(^|[\n ])((www|ftp)\.[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>", $ret );
+		$ret = preg_replace( "#(^|[\n ])((www|ftp)\.[\w\#$%&~/.\-;:=,?@\[\]+]*)#iU", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>", $ret );
 
 		// matches an email@domain type address at the start of a line, or after a space.
 		// Note: Only the followed chars are valid; alphanums, "-", "_" and or ".".
-		$ret = preg_replace( "#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $ret );
+		$ret = preg_replace( "#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#iU", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $ret );
 
 		// Remove our padding..
 		$ret = substr($ret, 1);
@@ -345,8 +345,8 @@ if( !function_exists( 'SULlyLoad' ) )
 				$readme = file_get_contents( WP_CONTENT_DIR . '/plugins/' . $itemname . '/readme.txt' );
 
 				$readme = preg_replace( "/.*\=\=.?change.?log.?\=\=/is", "", $readme );		// Remove everything above the changelog line.
-				$readme = preg_replace( "/$\s*\=\=.*/s", "", $readme );							// Remove any line that starts with a double =, like a title of some kind.
-				$readme = preg_replace( "/$\s*\*\*/s", "=", $readme ); 							// Some people use ** instead of = for their version log.
+				$readme = preg_replace( "/^\s*\=\=.*/s", "", $readme );							// Remove any line that starts with a double =, like a title of some kind.
+				$readme = preg_replace( "/^\s*\*\*/s", "=", $readme ); 							// Some people use ** instead of = for their version log.
 				$readme = preg_replace( "/\=.*.\=/", "", $readme, 1 );						// Remove the first single equal enclosed lines, aka version number.
 				$readme = preg_replace( "/\=.*/s", "", $readme );							// Remove everything from the next equal sign to the end of file.
 				$readme = trim( $readme );													// Trim the result to make it look nice.
